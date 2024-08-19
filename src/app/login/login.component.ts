d6cf1dd7940b16da1c7cd3663ba6264c7af6ae77
erellsworth <edward.ellsworth@gmail.com>
2024-08-19 08:08:25 -0400

    Starting pickle ui components

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../pickle-ui/button/button.component';
+import { TextInputComponent } from '../pickle-ui/text-input/text-input.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent, TextInputComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
---
+++ src/app/pickle-ui/text-area-input/text-area-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pickle-text-area-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-area-input.component.html',
+  styleUrl: './text-area-input.component.scss'
+})
+export class TextAreaInputComponent {
+
+}
