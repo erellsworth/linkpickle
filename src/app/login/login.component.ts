c86c8d3bc6c953672587723035223b0120bab96a
erellsworth <edward.ellsworth@gmail.com>
2024-08-23 13:02:40 -0400

    style login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
-import { ButtonComponent } from '../pickle-ui/button/button.component';
-import { TextInputComponent } from '../pickle-ui/text-input/text-input.component';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '../services/user.service';
+
+export interface LoginForm {
+  email: FormControl<string>;
+  password: FormControl<string>;
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ButtonComponent, TextInputComponent],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  public formGroup!: FormGroup<LoginForm>;
+
+  constructor(private fb: FormBuilder, private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.formGroup = this.fb.group({
+      email: this.fb.nonNullable.control('', Validators.required),
+      password: this.fb.nonNullable.control('', Validators.required)
+    })
+  }
+
+  async login(): Promise<void> {
+    console.log('login', this.formGroup.value);
+  }
 }
