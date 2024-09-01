a33df4fa30c7ea9f2098a99992ecbaea8908c097
erellsworth <edward.ellsworth@gmail.com>
2024-09-01 14:33:53 -0400

    Show categories in link card

--- src/app/dashboard/links/link-card/link-card.component.ts
+++ src/app/dashboard/links/link-card/link-card.component.ts
@@ -1,13 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { LpLink } from '../../../../../api/interfaces/link';
+import { faJar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-link-card',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, RouterLink, RouterLinkActive],
   templateUrl: './link-card.component.html',
-  styleUrl: './link-card.component.scss'
+  styleUrl: './link-card.component.scss',
 })
 export class LinkCardComponent {
-  @Input({ required: true }) link!: LpLink; 
+  @Input({ required: true }) link!: LpLink;
+
+  public icons = {
+    jar: faJar,
+  };
 }
