e7ed6ce1b43247a81e90cdd8f40d06048de489e4
erellsworth <edward.ellsworth@gmail.com>
2024-09-01 13:44:49 -0400

    Better stlying for nav bar

--- src/app/dashboard/components/navigation/navigation.component.ts
+++ src/app/dashboard/components/navigation/navigation.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CategoryService } from '../../../services/category.service';
 import { LpCategory } from '../../../../../api/interfaces/category';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHome, faJ, faJar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [RouterLink],
+  imports: [FontAwesomeModule, RouterLink],
   templateUrl: './navigation.component.html',
-  styleUrl: './navigation.component.scss'
+  styleUrl: './navigation.component.scss',
 })
-export class NavigationComponent{
+export class NavigationComponent {
+  public icons = {
+    home: faHome,
+    jar: faJar,
+  };
 
-  constructor(private categoryService: CategoryService) { }
+  constructor(private categoryService: CategoryService) {}
 
   public get categories(): LpCategory[] {
-    return this.categoryService.categories().filter(cat => Boolean(cat.id));
+    return this.categoryService.categories().filter((cat) => Boolean(cat.id));
   }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from './components/navigation/navigation.component';
-import {  RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { PageComponent } from '../pickle-ui/page/page.component';
 
 @Component({
@@ -8,8 +8,6 @@ import { PageComponent } from '../pickle-ui/page/page.component';
   standalone: true,
   imports: [NavigationComponent, PageComponent, RouterOutlet],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent  {
-
-}
+export class DashboardComponent {}
