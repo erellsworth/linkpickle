abc5fe606e451b7f95eaa84fb87095467180ca05
erellsworth <edward.ellsworth@gmail.com>
2024-09-02 12:50:45 -0400

    Show pinned status in link card

--- src/app/dashboard/links/link-card/link-card.component.ts
+++ src/app/dashboard/links/link-card/link-card.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { LpLink } from '../../../../../api/interfaces/link';
-import { faJar } from '@fortawesome/free-solid-svg-icons';
+import { faJar, faThumbTack } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TooltipComponent } from '../../../pickle-ui/tooltip/tooltip.component';
 
 @Component({
   selector: 'app-link-card',
   standalone: true,
-  imports: [FontAwesomeModule, RouterLink, RouterLinkActive],
+  imports: [FontAwesomeModule, TooltipComponent, RouterLink, RouterLinkActive],
   templateUrl: './link-card.component.html',
   styleUrl: './link-card.component.scss',
 })
@@ -16,5 +17,6 @@ export class LinkCardComponent {
 
   public icons = {
     jar: faJar,
+    pinned: faThumbTack,
   };
 }
