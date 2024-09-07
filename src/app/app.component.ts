65598327ada70d5955de1175646fd44b6b19e5fa
erellsworth <edward.ellsworth@gmail.com>
2024-09-07 12:58:38 -0400

    Add modal to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ToasterComponent } from "./pickle-ui/toaster/toaster.component";
+import { ToasterComponent } from './pickle-ui/toaster/toaster.component';
+import { ModalComponent } from './pickle-ui/modal/modal.component';
 
 @Component({
   selector: 'link-pickle',
   standalone: true,
-  imports: [RouterOutlet, ToasterComponent],
+  imports: [ModalComponent, RouterOutlet, ToasterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-
-
-}
+export class AppComponent {}
