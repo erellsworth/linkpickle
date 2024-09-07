69bd23e2f44ec43c71e885f0bfaa3ebf3e3839c8
erellsworth <edward.ellsworth@gmail.com>
2024-09-07 09:05:11 -0400

    Start modal

---
+++ src/app/pickle-ui/modal/modal.component.ts
@@ -0,0 +1,12 @@
+import { Component, contentChild } from '@angular/core';
+
+@Component({
+  selector: 'app-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './modal.component.html',
+  styleUrl: './modal.component.scss',
+})
+export class ModalComponent {
+  public headerContent = contentChild('header');
+}
