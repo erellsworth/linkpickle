f1257a5aa950b53adaa4c9312375f0f739885689
erellsworth <edward.ellsworth@gmail.com>
2024-10-15 08:37:19 -0400

    Add back button to link page

--- src/app/dashboard/link/link.component.ts
+++ src/app/dashboard/link/link.component.ts
@@ -3,15 +3,20 @@ import { LpLink } from '../../../../api/interfaces/link';
 import { LinkService } from '../../services/link.service';
 import { ToasterService } from '../../services/toaster.service';
 import { LinkCardComponent } from '../links/link-card/link-card.component';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavigationService } from '../../services/navigation.service';
+import { DashboardPageComponent } from '../components/dashboard-page/dashboard-page.component';
 
 @Component({
   selector: 'app-link',
   standalone: true,
-  imports: [LinkCardComponent],
+  imports: [DashboardPageComponent, FontAwesomeModule, LinkCardComponent],
   templateUrl: './link.component.html',
   styleUrl: './link.component.scss',
 })
 export class LinkComponent {
+  public backIcon = faArrowLeft;
   public link!: LpLink;
 
   @Input()
@@ -21,8 +26,13 @@ export class LinkComponent {
 
   constructor(
     private linkService: LinkService,
-    private toaster: ToasterService
+    private nav: NavigationService,
+    private toaster: ToasterService,
   ) {}
+
+  public goBack() {
+    this.nav.back();
+  }
 
   private async loadLink(id: number): Promise<void> {
     const link = await this.linkService.getLink(id);
